871eeb5827bd141c45ad4f2b837e41e7ccc4818e
TheEnderPrime <dbaugh.oregon@gmail.com>
2018-03-16 00:05:18 -0700

    added basic camera, first level of antioch, and notes page

---
+++ RPG Tutorial/Assets/Notes.cs
@@ -0,0 +1,9 @@
+﻿/*
+NOTES FOR THE FUTURE AND IDEAS TO DEVELOP
+
+ -  Have the camera change from a closer right behind the character inside 
+ 	to a higher up and away camera angle as soon as the character is outside
+
+
+
+*/
