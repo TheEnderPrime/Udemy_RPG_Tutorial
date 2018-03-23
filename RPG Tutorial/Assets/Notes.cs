f9dafaa066fd757f6746cb9796462d71c2142bc9
TheEnderPrime <dbaugh.oregon@gmail.com>
2018-03-23 09:58:33 -0700

    finished to video about notes

--- RPG Tutorial/Assets/Notes.cs
+++
@@ -1,9 +0,0 @@
-﻿/*
-NOTES FOR THE FUTURE AND IDEAS TO DEVELOP
-
- -  Have the camera change from a closer right behind the character inside 
- 	to a higher up and away camera angle as soon as the character is outside
-
-
-//TODO figure out how to smoothly transistion between inside view and outside view camera angles
-*/
