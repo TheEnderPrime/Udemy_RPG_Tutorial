5fab9f603bf6a4191e86af74f7d2d6fb4aeaa7ce
TheEnderPrime <dbaugh.oregon@gmail.com>
2018-05-29 16:35:10 -0700

    41, tags, starter prefabs, more terrain changes

--- RPG Tutorial/Assets/Camera & UI/CameraRaycaster.cs
+++ RPG Tutorial/Assets/Camera & UI/CameraRaycaster.cs
@@ -53,6 +53,7 @@ public class CameraRaycaster : MonoBehaviour
         // Otherwise return background hit
         raycastHit.distance = distanceToBackground;
         layerHit = Layer.RaycastEndStop;
+        onLayerChange(layerHit);
     }
 
     RaycastHit? RaycastForLayer(Layer layer)
