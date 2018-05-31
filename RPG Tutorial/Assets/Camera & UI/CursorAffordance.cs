e3e16a087fd75a9e7fe42a7833e86d3e47574abb
TheEnderPrime <dbaugh.oregon@gmail.com>
2018-05-30 18:41:29 -0700

    51, enemy pathfinding, player pathfinding

--- RPG Tutorial/Assets/Camera & UI/CursorAffordance.cs
+++ RPG Tutorial/Assets/Camera & UI/CursorAffordance.cs
@@ -2,36 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent (typeof(CameraRaycaster))]
+[RequireComponent(typeof(CameraRaycaster))]
 public class CursorAffordance : MonoBehaviour {
 
-	[SerializeField] Texture2D walkCursor = null;
-	[SerializeField] Texture2D targetCursor = null;
-	[SerializeField] Texture2D unknownCursor = null;
-	[SerializeField] Vector2 cursorHotspot = new Vector2(0, 0);
-	CameraRaycaster cameraRaycaster;
+    [SerializeField] Texture2D walkCursor = null;
+    [SerializeField] Texture2D unknownCursor = null;
+    [SerializeField] Texture2D targetCursor = null;
+    [SerializeField] Vector2 cursorHotspot = new Vector2(0, 0);
+
+    // TODO solve fight between serialize and const
+    [SerializeField] const int walkableLayerNumber = 8;
+    [SerializeField] const int enemyLayerNumber = 9;
+
+    CameraRaycaster cameraRaycaster;
 
 	// Use this for initialization
 	void Start () {
-		cameraRaycaster = GetComponent<CameraRaycaster>();
-		cameraRaycaster.onLayerChange += OnLayerChanged; //registering
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+        cameraRaycaster.notifyLayerChangeObservers += OnLayerChanged; // registering
 	}
-	
-	// Update is only called when layer changes
-	void OnLayerChanged (Layer newLayer) {
-		switch(newLayer){
-			case Layer.Walkable:
-				Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);		
-				break;
-			case Layer.Enemy:
-				Cursor.SetCursor(targetCursor, cursorHotspot, CursorMode.Auto);
-				break;
-			case Layer.RaycastEndStop:
-				Cursor.SetCursor(unknownCursor, cursorHotspot, CursorMode.Auto);
-				break;
-			default:
-				Debug.LogError("Don't know what cursor to show");
-				return;
-		}
-	}
+
+    void OnLayerChanged(int newLayer) {
+        print("Cursor over new layer");
+        switch (newLayer)
+        {
+            case walkableLayerNumber:
+                Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case enemyLayerNumber:
+                Cursor.SetCursor(targetCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            default:
+                Cursor.SetCursor(unknownCursor, cursorHotspot, CursorMode.Auto);
+                return;
+        }
+    }
+
+    // TODO consider de-registering OnLayerChanged on leaving all game scenes
 }
--- RPG Tutorial/Assets/Player/PlayerMovement.cs
+++ RPG Tutorial/Assets/Player/PlayerMovement.cs
@@ -1,82 +1,85 @@
 using System;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityStandardAssets.Characters.ThirdPerson;
 
 [RequireComponent(typeof (ThirdPersonCharacter))]
+[RequireComponent(typeof (NavMeshAgent))]
+[RequireComponent(typeof (AICharacterControl))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float walkMoveStopRadius = 0.2f;
-    [SerializeField] float attackMoveStopRadius = 5f;
-    ThirdPersonCharacter ThirdPersonCharacter;   // A reference to the ThirdPersonCharacter on the object
-    CameraRaycaster cameraRaycaster;
+    ThirdPersonCharacter ThirdPersonCharacter = null;   // A reference to the ThirdPersonCharacter on the object
+    CameraRaycaster cameraRaycaster = null;
     Vector3 currentDestination, clickPoint;
+    AICharacterControl aiCharacterControl = null;
+    GameObject walkTarget = null;
+
+    // TODO solve fight between serialize and const
+    [SerializeField] const int walkableLayerNumber = 8;
+    [SerializeField] const int enemyLayerNumber = 9;
 
     bool isInDirectMode = false;
     private bool Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
 
 
-    private void Start()
+    void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
         ThirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
         currentDestination = transform.position;
+        aiCharacterControl = GetComponent<AICharacterControl>();
+        walkTarget = new GameObject("walkTarget");
+
+        cameraRaycaster.notifyMouseClickObservers += ProcessMouseClick;
     }
 
-    private void Update()
-        {
-            if (!Jump)
-            {
-                Jump = Input.GetButtonDown("Jump");
-            }
+    void ProcessMouseClick(RaycastHit raycastHit, int layerHit) {
+        switch(layerHit) {
+            case enemyLayerNumber:
+                GameObject enemy = raycastHit.collider.gameObject;
+                aiCharacterControl.SetTarget(enemy.transform);
+                break;
+            case walkableLayerNumber:
+                walkTarget.transform.position = raycastHit.point;
+                aiCharacterControl.SetTarget(walkTarget.transform);
+                break;
+            default:
+                Debug.LogWarning("Mouse Click Handling Error in Player Movement");
+                return;
         }
+    }
+
+    // private void Update()
+    // {
+    //         if (!Jump)
+    //         {
+    //             Jump = Input.GetButtonDown("Jump");
+    //         }
+    // }
 
     // Fixed update is called in sync with physics
-    private void FixedUpdate()
-    {
-        if(Input.GetKeyDown(KeyCode.G)) // G for gamepad
-        {
-            isInDirectMode = !isInDirectMode; //toggle mode
-            currentDestination = transform.position; //clear the click target
-        }
+    // private void FixedUpdate()
+    // {
+    //     if(Input.GetKeyDown(KeyCode.G)) // G for gamepad
+    //     {
+    //         isInDirectMode = !isInDirectMode; //toggle mode
+    //         currentDestination = transform.position; //clear the click target
+    //     }
 
-        if(isInDirectMode) {
+    //     if(isInDirectMode) {
             
-            ProcessDirectMovement();
+    //         ProcessDirectMovement();
         
-        } else {
-            ProcessMouseMovement(); //mouse movement
-        }
+    //     } else {
+    //         ProcessMouseMovement(); //mouse movement
+    //     }
 
         
-    }
+    // }
 
-    private void ProcessMouseMovement()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            clickPoint = cameraRaycaster.hit.point;
-            switch (cameraRaycaster.currentLayerHit)
-            {
-                case Layer.Walkable:
-                    currentDestination = clickPoint;  // So not set in default case
-                    currentDestination = ShortDestination(clickPoint, walkMoveStopRadius);
-
-                    break;
-                case Layer.Enemy:
-                    currentDestination = ShortDestination(clickPoint, attackMoveStopRadius);
-                    //currentClickTarget = cameraRaycaster.hit.point;
-                    break;
-                default:
-                    print("Unexpected Layer Found");
-                    return;
-            }
-        }
-
-        WalkToDestination();
-    }
 
     
-
+// TODO make this get called once again
     private void ProcessDirectMovement() 
     {
         float h = Input.GetAxis("Horizontal");
@@ -92,35 +95,10 @@ public class PlayerMovement : MonoBehaviour
         Jump = false;
     }
 
-    private void WalkToDestination()
-    {
-        var playerToClickPoint = currentDestination - transform.position;
-        if (playerToClickPoint.magnitude >= walkMoveStopRadius)
-        {
-            ThirdPersonCharacter.Move(playerToClickPoint, false, false);
-        }
-        else
-        {
-            ThirdPersonCharacter.Move(Vector3.zero, false, false);
-        }
-    }
-
     Vector3 ShortDestination(Vector3 destination, float shortening)
     {
         Vector3 reductionVector = (destination - transform.position).normalized * shortening;
         return destination - reductionVector;
-    }
-
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.black;
-        Gizmos.DrawLine(transform.position, currentDestination);
-        Gizmos.DrawSphere(currentDestination, 0.1f); // shows shortened destination
-        Gizmos.DrawSphere(clickPoint, 0.15f); // shows click point
-
-        //Draw attack sphere
-        Gizmos.color = new Color(255f, 0f, 0, .5f);
-        Gizmos.DrawWireSphere(transform.position, attackMoveStopRadius);
     }
 }
 
