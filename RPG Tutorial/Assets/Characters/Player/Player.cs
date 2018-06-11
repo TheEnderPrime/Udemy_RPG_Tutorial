39e126c07c452eb7abc39cbf73deffc05115b57d
TheEnderPrime <dbaugh.oregon@gmail.com>
2018-06-11 09:32:49 -0700

    83, added weapon changing

--- RPG Tutorial/Assets/Characters/Player/Player.cs
+++ RPG Tutorial/Assets/Characters/Player/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Player : MonoBehaviour, IDamageable
 {
@@ -11,6 +12,7 @@ public class Player : MonoBehaviour, IDamageable
     [SerializeField] float damagePerHit = 10f;
     [SerializeField] float minTimeBetweenHits = 0.5f;
     [SerializeField] float maxAttackRange = 2f;
+    
     [SerializeField] Weapon weaponInUse;
 
     GameObject currentTarget;
@@ -30,8 +32,20 @@ public class Player : MonoBehaviour, IDamageable
     private void PutWeaponInHand()
     {
 		var weaponPrefab = weaponInUse.GetWeaponPrefab();
-		var weapon = Instantiate(weaponPrefab);
-		// TODO move to correct place and child to hand
+        GameObject dominantHand = RequestDominantHand();
+		var weapon = Instantiate(weaponPrefab, dominantHand.transform);
+        weapon.transform.localPosition = weaponInUse.gripTransform.localPosition;
+        weapon.transform.localRotation = weaponInUse.gripTransform.localRotation; 
+    }
+
+    private GameObject RequestDominantHand()
+    {
+        var dominantHands = GetComponentsInChildren<DominantHand>();
+        int numberOfDominantHands = dominantHands.Length;
+        Assert.AreNotEqual(numberOfDominantHands, 0, "No DominantHand found on player, please add one");
+        Assert.IsFalse(numberOfDominantHands > 1, "Multiple DominantHand scripts on Player, please remove one");
+
+        return dominantHands[0].gameObject;
     }
 
     private void RegisterForMouseClick()
