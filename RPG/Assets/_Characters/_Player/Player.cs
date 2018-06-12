75b9549a65144044a337c3a1bc32dee2301f71af
TheEnderPrime <dbaugh.oregon@gmail.com>
2018-06-12 13:44:19 -0700

    98, began adding dialogue

---
+++ RPG/Assets/_Characters/_Player/Player.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using RPG.CameraUI; // TODO consider re-wiring
+using RPG.Weapons;
+using RPG.Core;
+
+namespace RPG.Characters
+{
+    public class Player : MonoBehaviour, IDamageable
+    {
+
+        [SerializeField] float maxHealthPoints = 100f;
+        [SerializeField] int enemyLayer = 9;
+        [SerializeField] float damagePerHit = 10f;
+        [SerializeField] Weapon weaponInUse;
+        [SerializeField] AnimatorOverrideController animatorOverrideController;
+
+        Animator animator;
+        float currentHealthPoints;
+        CameraRaycaster cameraRaycaster;
+        float lastHitTime = 0f;
+
+        public float healthAsPercentage { get { return currentHealthPoints / maxHealthPoints; } }
+
+        void Start()
+        {
+            RegisterForMouseClick();
+            SetCurrentMaxHealth();
+            PutWeaponInHand();
+            SetupRuntimeAnimator();
+        }
+
+        public void TakeDamage(float damage)
+        {
+            currentHealthPoints = Mathf.Clamp(currentHealthPoints - damage, 0f, maxHealthPoints);
+        }
+
+        private void SetCurrentMaxHealth()
+        {
+            currentHealthPoints = maxHealthPoints;
+        }
+
+        private void SetupRuntimeAnimator()
+        {
+            animator = GetComponent<Animator>();
+            animator.runtimeAnimatorController = animatorOverrideController;
+            animatorOverrideController["DEFAULT ATTACK"] = weaponInUse.GetAttackAnimClip(); // TODO remove const
+        }
+
+        private void PutWeaponInHand()
+        {
+            var weaponPrefab = weaponInUse.GetWeaponPrefab();
+            GameObject dominantHand = RequestDominantHand();
+            var weapon = Instantiate(weaponPrefab, dominantHand.transform);
+            weapon.transform.localPosition = weaponInUse.gripTransform.localPosition;
+            weapon.transform.localRotation = weaponInUse.gripTransform.localRotation;
+        }
+
+        private GameObject RequestDominantHand()
+        {
+            var dominantHands = GetComponentsInChildren<DominantHand>();
+            int numberOfDominantHands = dominantHands.Length;
+            Assert.AreNotEqual(numberOfDominantHands, 0, "No DominantHand found on player, please add one");
+            Assert.IsFalse(numberOfDominantHands > 1, "Multiple DominantHand scripts on Player, please remove one");
+
+            return dominantHands[0].gameObject;
+        }
+
+        private void RegisterForMouseClick()
+        {
+            cameraRaycaster = FindObjectOfType<CameraRaycaster>();
+            cameraRaycaster.notifyMouseClickObservers += OnMouseClick;
+        }
+
+        void OnMouseClick(RaycastHit raycastHit, int layerHit)
+        {
+            if (layerHit == enemyLayer)
+            {
+                var enemy = raycastHit.collider.gameObject;
+                if (IsTargetInRange(enemy))
+                {
+                    AttackTarget(enemy);
+                }
+            }
+        }
+
+        private void AttackTarget(GameObject target)
+        {
+            var enemyComponent = target.GetComponent<Enemy>();
+            if (Time.time - lastHitTime > weaponInUse.GetMinTimeBetweenHits())
+            {
+                animator.SetTrigger("Attack"); //TODO make const;
+                enemyComponent.TakeDamage(damagePerHit);
+                lastHitTime = Time.time;
+            }
+        }
+
+        private bool IsTargetInRange(GameObject target)
+        {
+            float distanceToTarget = (target.transform.position - transform.position).magnitude;
+            return distanceToTarget <= weaponInUse.GetMaxAttackRange();
+        }
+
+    }
+}
--- RPG/Assets/_Weapons/Weapon.cs
+++ RPG/Assets/_Weapons/Weapon.cs
@@ -12,6 +12,18 @@ namespace RPG.Weapons
 
         [SerializeField] GameObject weaponPrefab;
         [SerializeField] AnimationClip attackAnimation;
+        [SerializeField] float minTimeBetweenHits = 0.5f;
+        [SerializeField] float maxAttackRange = 2f;
+
+        public float GetMinTimeBetweenHits()
+        {
+            return minTimeBetweenHits;
+        }
+
+        public float GetMaxAttackRange()
+        {
+            return maxAttackRange;
+        }
 
         public GameObject GetWeaponPrefab()
         {
