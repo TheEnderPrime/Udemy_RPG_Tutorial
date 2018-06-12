34950d6a28b978ff214fa302edf8d4ad63934db4
TheEnderPrime <dbaugh.oregon@gmail.com>
2018-06-12 11:53:55 -0700

    94, added massive animation pack, got rid of standard assets folder

---
+++ RPG/Assets/_Weapons/Weapon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Weapons
+{
+    [CreateAssetMenu(menuName = ("RPG/Weapon"))]
+    public class Weapon : ScriptableObject
+    {
+
+        public Transform gripTransform;
+
+        [SerializeField] GameObject weaponPrefab;
+        [SerializeField] AnimationClip attackAnimation;
+
+        public GameObject GetWeaponPrefab()
+        {
+            return weaponPrefab;
+        }
+
+        public AnimationClip GetAttackAnimClip()
+        {
+            RemoveAnimationEvents();
+            return attackAnimation;
+        }
+
+        // So that asset packs cannot cause crashes
+        private void RemoveAnimationEvents()
+        {
+            attackAnimation.events = new AnimationEvent[0];
+        }
+    }
+}
